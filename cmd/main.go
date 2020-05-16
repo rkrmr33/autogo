7e4578d1c6f1de2523ea17bb37aabfdafa36c91f
Roi Kramer <roi.kramer@codefresh.io>
2020-05-16 14:53:58 +0300

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	
+
 	"github.com/roikramer120/autogo/pkg/slacknotify"
 )
 
 func main() {
-	fmt.Println("test")
-	sn, err := slacknotify.Init(nil)
-	sn.SendString("Hello channel")
+	slacknotify.Init(nil)
 }
