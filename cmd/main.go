292257d1e6b7d5a24aad10cbedb0f573430fef28
Roi Kramer <roi.kramer@codefresh.io>
2020-05-16 14:42:20 +0300

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"time"
-
-	"github.com/go-vgo/robotgo"
-	hooks "github.com/robotn/gohook"
+	
+	"github.com/roikramer120/autogo/pkg/slacknotify"
 )
 
 func main() {
-	interval := flag.Int("i", 1000, "The click interval in milliseconds (default 1000 ms)")
-	startChan := make(chan interface{})
-	pause := true
-	flag.Parse()
-	fmt.Println("Press [k] to start, send any signal to stop...")
-
-	go func() {
-		evChan := hooks.Start()
-
-		for ev := range evChan {
-			if ev.Rawcode == 40 && ev.Kind == hooks.KeyUp {
-				if pause {
-					pause = false
-					startChan <- struct{}{}
-				} else {
-					pause = true
-				}
-			}
-		}
-	}()
-
-	for {
-		<-startChan
-		fmt.Printf("Right clicking every %v ms\n", *interval)
-		for !pause {
-			time.Sleep(time.Duration(*interval) * time.Millisecond)
-			fmt.Println("right clicking...")
-			robotgo.Click("right", false)
-		}
-		fmt.Println("Paused")
-	}
+	fmt.Println("test")
+	sn, err := slacknotify.Init(nil)
+	sn.SendString("Hello channel")
 }
